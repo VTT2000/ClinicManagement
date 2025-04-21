2e2141cecdcc63eaf7da4008521f3cabda74b7f8
VuuThuan <thuancmag2000@gmail.com>
2025-04-21 20:21:45 +0700

    updated2updated2

--- CaptonseProject/Infrastructure/Services/UserService.cs
+++ CaptonseProject/Infrastructure/Services/UserService.cs
@@ -7,80 +7,80 @@ using web_api_base.Models.ViewModel;
 
 public interface IUserService
 {
-  Task<IEnumerable<User>> GetAllAsync();
-  Task<dynamic> Register(UserRegisterVM newUser);
+    Task<IEnumerable<User>> GetAllAsync();
+    Task<dynamic> Register(UserRegisterVM newUser);
 
-  Task<dynamic> AdminCreateUser(AdminRegisterUserVM newUser);
+    Task<dynamic> AdminCreateUser(AdminRegisterUserVM newUser);
 
-  UserLoginResultVM Login(UserLoginVM userLogin);
+    UserLoginResultVM Login(UserLoginVM userLogin);
 
-  public Task<HTTPResponseClient<IEnumerable<User>>> GetAllUserIsStaffAsync();
-  public Task<HTTPResponseClient<IEnumerable<User>>> GetAllUserIsPatientAsync();
+    public Task<HTTPResponseClient<IEnumerable<User>>> GetAllUserIsStaffAsync();
+    public Task<HTTPResponseClient<IEnumerable<User>>> GetAllUserIsPatientAsync();
 }
 
 public class UserService : IUserService
 {
-  public IUnitOfWork _unitOfWork;
-  public JwtAuthService _JwtAuthService;
+    public IUnitOfWork _unitOfWork;
+    public JwtAuthService _JwtAuthService;
 
-  public UserService(IUnitOfWork unitOfWork, JwtAuthService JwtAuthService)
-  {
-    _unitOfWork = unitOfWork;
-    _JwtAuthService = JwtAuthService;
-  }
-
-  public async Task<dynamic> AdminCreateUser(AdminRegisterUserVM newUser)
-  {
-    var user = new User()
+    public UserService(IUnitOfWork unitOfWork, JwtAuthService JwtAuthService)
     {
-      FullName = newUser.FullName,
-      Email = newUser.Email,
-      PasswordHash = PasswordHelper.HashPassword(newUser.Password),
-      Role = newUser.Role,
-    };
-    await _unitOfWork._userRepository.AddAsync(user);
-    await _unitOfWork.SaveChangesAsync();
-    return user;
-  }
-
-  public async Task<IEnumerable<User>> GetAllAsync()
-  {
-    var users = await _unitOfWork._userRepository.GetAllAsync();
-    return users;
-  }
-
-  public UserLoginResultVM Login(UserLoginVM userLogin)
-  {
-    //Tìm user trong db có  email
-    var userCheckLogin = _unitOfWork._userRepository.GetAllAsync().Result.FirstOrDefault(x => x.Email == userLogin.Account);
-    //Nếu account tồn tại trong db
-    if (userCheckLogin != null && PasswordHelper.VerifyPassword(userLogin.Password, userCheckLogin.PasswordHash))
-    {
-      //Tạo token cho user
-      var token = _JwtAuthService.GenerateToken(userCheckLogin);
-      UserLoginResultVM usLoginResult = new UserLoginResultVM();
-      usLoginResult.AccessToken = token;
-      usLoginResult.User = userCheckLogin;
-      return usLoginResult;
+        _unitOfWork = unitOfWork;
+        _JwtAuthService = JwtAuthService;
     }
-    return null;
-  }
 
-  public async Task<dynamic> Register(UserRegisterVM newUser)
-  {
-    var user = new User()
+    public async Task<dynamic> AdminCreateUser(AdminRegisterUserVM newUser)
     {
-      FullName = newUser.FullName,
-      Email = newUser.Email,
-      PasswordHash = PasswordHelper.HashPassword(newUser.Password),
-      Role = RoleConstant.User,
-    };
-    await _unitOfWork._userRepository.AddAsync(user);
-    await _unitOfWork.SaveChangesAsync();
-    return user;
-  }
+        var user = new User()
+        {
+            FullName = newUser.FullName,
+            Email = newUser.Email,
+            PasswordHash = PasswordHelper.HashPassword(newUser.Password),
+            Role = newUser.Role,
+        };
+        await _unitOfWork._userRepository.AddAsync(user);
+        await _unitOfWork.SaveChangesAsync();
+        return user;
+    }
 
-  public async Task<HTTPResponseClient<IEnumerable<User>>> GetAllUserIsStaffAsync()
+    public async Task<IEnumerable<User>> GetAllAsync()
+    {
+        var users = await _unitOfWork._userRepository.GetAllAsync();
+        return users;
+    }
+
+    public UserLoginResultVM Login(UserLoginVM userLogin)
+    {
+        //Tìm user trong db có  email
+        var userCheckLogin = _unitOfWork._userRepository.GetAllAsync().Result.FirstOrDefault(x => x.Email == userLogin.Account);
+        //Nếu account tồn tại trong db
+        if (userCheckLogin != null && PasswordHelper.VerifyPassword(userLogin.Password, userCheckLogin.PasswordHash))
+        {
+            //Tạo token cho user
+            var token = _JwtAuthService.GenerateToken(userCheckLogin);
+            UserLoginResultVM usLoginResult = new UserLoginResultVM();
+            usLoginResult.AccessToken = token;
+            usLoginResult.User = userCheckLogin;
+            return usLoginResult;
+        }
+        return null;
+    }
+
+    public async Task<dynamic> Register(UserRegisterVM newUser)
+    {
+        var user = new User()
+        {
+            FullName = newUser.FullName,
+            Email = newUser.Email,
+            PasswordHash = PasswordHelper.HashPassword(newUser.Password),
+            Role = RoleConstant.User,
+        };
+        await _unitOfWork._userRepository.AddAsync(user);
+        await _unitOfWork.SaveChangesAsync();
+        return user;
+    }
+
+    public async Task<HTTPResponseClient<IEnumerable<User>>> GetAllUserIsStaffAsync()
     {
         var result = new HTTPResponseClient<IEnumerable<User>>();
         try
